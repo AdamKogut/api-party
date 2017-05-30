8028e74f1540db66f5af2b05731630e78b27f091
AdamKogut <akogut@purdue.edu>
2017-05-30 15:53:07 -0400

    Push to new location on submit

--- src/Github.js
+++ src/Github.js
@@ -12,11 +12,15 @@ class Github extends Component{
         this.setState({username})
     }
 
+    handleSubmit=(ev)=>{
+        ev.preventDefault()
+        this.props.history.push(`/github/${this.state.username}`)
+    }
     render(){
         return(
             <div className="github">
                 <img className="github-logo"src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="Github logo"/>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input type="text" value={this.state.username} onChange={this.handleChange}/>
                     </div>
